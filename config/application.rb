57591e738a5355b3cf683fa20a15774bb3096d9e
John Calvin <calvjd@gmail.com>
2013-11-14 20:40:56 -0800

    assets

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'csv'
 require 'rails/all'
-require 'sprockets/railtie'
+#require 'sprockets/railtie'
 
 
 # Require the gems listed in Gemfile, including any gems
@@ -23,5 +23,6 @@ module WriteupApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 		config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+        config.assets.precompile << '*.js'
   end
 end
