41668fd6b559609f7da462a3525897ee951d0ede
John Calvin <calvjd@gmail.com>
2013-11-17 21:09:56 -0800

    push to correct assets

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module WriteupApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 		config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-        #config.assets.precompile << '*.js'
+        config.assets.precompile << '*.js'
   end
 end
