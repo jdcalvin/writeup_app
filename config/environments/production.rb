451ac53cc62f7d90d47c8802c9b9bb5e06ec9f13
John Calvin <calvjd@gmail.com>
2013-12-03 20:56:30 -0800

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ WriteupApp::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.initialize_on_precompile = true #Added this
 
   # Generate digests for assets URLs.
   config.assets.digest = true
