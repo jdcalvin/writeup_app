5027d2ba36538ba4fe3c802bebbee14a3c24a465
John Calvin <calvjd@gmail.com>
2014-02-23 11:39:51 -0800

    Clearing assets to fixed heroku caching issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ WriteupApp::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = false => true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -28,13 +28,12 @@ WriteupApp::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.initialize_on_precompile = true #Added this
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '2.0'
+  config.assets.version = '3.0'
 
   # Specifies the header that your server uses for sing files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
